2fb02a96515476792c6716154e1f1657d0d1743c
vplacido <valentinplacido25@gmail.com>
2020-03-09 15:13:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100000);
+let mod = 16 % 6;
+let max = Math.max(1,20);
